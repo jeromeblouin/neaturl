cab281209691b8d8fdc549440ab74cc8301604cb
jeromeblouin <jeromeblou@gmail.com>
2025-11-05 17:08:37 -0500

    Added validation

--- src/main/java/org/neaturl/api/EncoderController.java
+++ src/main/java/org/neaturl/api/EncoderController.java
@@ -1,8 +1,11 @@
 package org.neaturl.api;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.extern.slf4j.Slf4j;
 import org.neaturl.service.UrlEncoderStrategy;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Validated
 @Slf4j
 public class EncoderController {
 
@@ -20,13 +24,16 @@ public class EncoderController {
     }
 
     @GetMapping("encode")
-    public ResponseEntity<String> encode(@RequestParam String url) {
+    public ResponseEntity<String> encode(
+            @RequestParam
+            @NotBlank @Pattern(regexp = "^https?://.*", message = "Must be a valid URL")
+            String url) {
         log.debug("URL to encode: {}", url);
         return ResponseEntity.ok(encoder.encode(url));
     }
 
     @GetMapping("decode")
-    public ResponseEntity<String> decode(@RequestParam String url) {
+    public ResponseEntity<String> decode(@RequestParam @NotBlank String url) {
         log.debug("URL to decode: {}", url);
         var decodedUrl = encoder.decode(url);
         if (decodedUrl.isPresent()) {
---
+++ src/main/java/org/neaturl/api/GlobalValidationHandler.java
@@ -0,0 +1,21 @@
+package org.neaturl.api;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalValidationHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List<String>> handleConstraintViolation(ConstraintViolationException ex) {
+        var errors = ex.getConstraintViolations().stream()
+                .map(v -> v.getPropertyPath() + ": " + v.getMessage())
+                .toList();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+}
